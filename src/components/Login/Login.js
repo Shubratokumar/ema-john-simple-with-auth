28a6d96ef83868933673907e2268d55a8756aefb
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-10 17:44:09 +0600

    requier auth implemented

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import "./Login.css";
 import auth from './../../firebase.init';
@@ -14,6 +14,8 @@ const Login = () => {
     error
   ] = useSignInWithEmailAndPassword(auth);
   const navigate = useNavigate();
+  const location = useLocation()
+  const from = location?.state?.from?.pathname || '/';
 
   const handleEmailBlur = event =>{
     setEmail(event.target.value);
@@ -23,7 +25,7 @@ const Login = () => {
   }
 
   if(user){
-      navigate('/shop');
+      navigate(from, { replace : true });
   }
 
   const handleUserSignIn = event =>{
@@ -48,7 +50,7 @@ const Login = () => {
           {
             loading && <p style={{color : 'greenyellow'}}>Loading...</p>
           }
-          <input className="form-submit" type="submit" value="Login" />
+          <input  className="form-submit" type="submit" value="Login" />
         </form>
         <p className="form-query">
           New to Ema-John?{" "}
